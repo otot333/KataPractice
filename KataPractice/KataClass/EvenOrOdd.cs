517f2f2d1fae00b3a617d2e39c59c2a70f464434
Oakley <Oakley>
2017-09-12 13:45:38 +0800

    Even or Odd Kata

---
+++ KataPractice/KataClass/EvenOrOdd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataPractice.KataClass
+{
+    public class EvenOrOdd
+    {
+        public string IsEvenOrOdds(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+            
+        }
+
+    }
+}
---
+++ KataPracticeTests/KataClass/EvenOrOddTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataPractice.KataClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataPractice.KataClass.Tests
+{
+    [TestClass()]
+    public class EvenOrOddTests
+    {
+        [TestMethod()]
+        public void IsEvenOrOddsTest_Ifnumberis0_ThenThisisEven()
+        {
+            var number = 0;
+            var EvenOdd = new EvenOrOdd();
+            string expected = "Even";
+            var actual = EvenOdd.IsEvenOrOdds(number);
+
+            Assert.AreSame(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsEvenOrOddsTest_Ifnumberis1_ThenThisisOdd()
+        {
+            var number = 1;
+            var EvenOdd = new EvenOrOdd();
+            string expected = "Odd";
+            var actual = EvenOdd.IsEvenOrOdds(number);
+
+            Assert.AreSame(expected, actual);
+        }
+    }
+}
